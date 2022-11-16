62658e7df5aafa10b4824c1f54b523fa54e7a630
Andres Melendes <andresruse18@gmail.com>
2022-11-16 20:25:38 +0800

    change user role name

--- kumbio_api/settings.py
+++ kumbio_api/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-41nwz$6_#7(+h)-#6ek7dkuwnvz_+p!ft=ayw=sktx-5q7wd7s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["api.kumbio.com"]
+CSRF_TRUSTED_ORIGINS = ['https://api.kumbio.com']
 
 # Application definition
 
