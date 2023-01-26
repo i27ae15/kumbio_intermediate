0a8237f642fd96ae7c9337c9810246b57d83e580
Andres Melendes <andresruse18@gmail.com>
2023-01-26 22:09:32 +0800

    before merge

--- kumbio_api/settings.py
+++ kumbio_api/settings.py
@@ -86,6 +86,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://192.168.1.8:5173",
     "https://app.kumbio.com",
     "https://booking.kumbio.com",
+    "https://subtle-raindrop-c72beb.netlify.app"
+    
 ]
 
 MIDDLEWARE = [
